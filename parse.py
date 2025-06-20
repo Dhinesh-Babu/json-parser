4d179b9e3371fe6d4621e5b543b4b644095d5d3b
Dhinesh Babu <dhinesh.rbabu@gmail.com>
2025-06-20 15:57:30 -0700

    feat: tokenizer complete

--- parse.py
+++ parse.py
@@ -6,6 +6,7 @@ def tokenize(json_string:str) -> list[str]:
 
     while i<n:
         char = json_string[i]
+
         # whitespace
         if char.isspace():
             i+=1
@@ -13,22 +14,70 @@ def tokenize(json_string:str) -> list[str]:
 
         # single-character / punctuations
         if char in '{}[],:':
+#_COMMENT             print("Found Special:\t",char)
             token.append(char)
             i+=1
             continue
         
         # strings
         if char == '"':
-            #TODO I think we need some kind of loop.
-            print("TBI")
+
+            string = '"'
+            i+=1
+            while i < n and json_string[i] !='"':
+                string+= json_string[i]
+                i+=1
+
+            if i >= n:
+                raise ValueError("Unterminated string",json_string)
+            
+            # ending '"' 
+            string+= json_string[i]
+            i+=1
+
+            token.append(string)
+#_COMMENT             print("Found String:\t",string)
             continue
         
         # numbers
-        if char.isdigit():
-            #TODO
-            print("TBI")
-        
+        if char.isdigit() or char == "-":
+
+            number = char
+            i+=1
+            # This is a tokenizer only. We should worry about the double '.' or '+' in the middle in the parser stage.
+            while i<n and (json_string[i].isdigit() or json_string[i] in ".+-"):
+                number+=json_string[i]
+                i+=1
+#_COMMENT             print("Found Number:\t",number)
+
+            token.append(number)
+            continue
+
         # literals
-            #TODO i think if we get char done we can just use the same logic.
+
+        if char.isalpha():
+
+            literal = ''
+
+            while i < n and json_string[i].isalpha():
+                literal+= json_string[i]
+                i+=1
+            if literal in ["true","false","null"]:
+#_COMMENT                 print("Found Literal:\t",literal)
+                token.append(literal)
+            else:
+                raise ValueError("Invalid Literal")
+            
+            continue
             
     return token
+
+
+# Simple Test
+test_string = '{ "key" : "value", "nums" : [1, -2.5, true, null] }'
+print(f"Tokenizing the string: {test_string}\n")
+try:
+    result_tokens = tokenize(test_string)
+    print(f"Resulting tokens: {result_tokens}")
+except ValueError as e:
+    print(f"Error: {e}")
